750be74196bc253518ef74c56c33cc8fedab91e3
Karlo Skokic <ks4898@g.rit.edu>
2025-03-22 18:44:29 +0100

    sum

---
+++ src/main/java/edu/rit/ks4898/App.java
@@ -0,0 +1,13 @@
+package edu.rit.ks4898;
+
+import io.javalin.Javalin;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.server;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
